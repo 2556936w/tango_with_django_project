b403453854f77029700ef59c7a29b9aef9bdf548
Timothy Wang <2556936w@student.gla.ac.uk>
2022-01-20 16:21:32 +0000

    Create about page, add hyperlinks in both index and about pages

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    message = "Rango says hey there partner!<br><a href='/rango/about/'>About</a>"
+    return HttpResponse(message)
+
+def about(request):
+    message = "Rango says this is the about page.<br><a href='/rango/'>Index</a>"
+    return HttpResponse(message)
