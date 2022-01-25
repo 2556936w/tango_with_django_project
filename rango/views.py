2cb6424ed92aa1c674de7ecacfe4f80bfff5c635
Timothy Wang <2556936w@student.gla.ac.uk>
2022-01-25 19:14:30 +0000

    Create templates dir, add index, implement in views, change settings

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    message = "Rango says hey there partner!<br><a href='/rango/about/'>About</a>"
-    return HttpResponse(message)
+    # Construct a dictionary to pass to the template engine as its context
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     message = "Rango says here is the about page.<br><a href='/rango/'>Index</a>"
