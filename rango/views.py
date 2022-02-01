cf9f3cd8c6a1802e1524add40f763eb59272d7fa
Timothy Wang <2556936w@student.gla.ac.uk>
2022-02-01 17:05:58 +0000

    Add categories list to index template, update views

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context
-    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Query database for a list of all categories currently stored sorted by number of likes (descending), pick top 5
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    # Create context dictionary containing the query results
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     # Return a rendered response to send to the client
     return render(request, 'rango/index.html', context=context_dict)
 
