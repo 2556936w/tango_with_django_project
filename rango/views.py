78cff5f1b31339ca31d6dde739274fd559c8e8f9
Timothy Wang <2556936w@student.gla.ac.uk>
2022-02-10 21:10:31 +0000

    Create restricted view, map url and add to base temp, add to settings

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
@@ -148,3 +149,8 @@ def user_login(request):
     else:
         # Blank dictionary object
         return render(request, 'rango/login.html')
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
